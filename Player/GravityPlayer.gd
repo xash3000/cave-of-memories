e4ff3af5c548c07c58371c4189b8ce866c66efd2
Ali Faki <alifaki077@gmail.com>
2020-08-09 17:17:02 +0200

    gravity affects player

--- Player/GravityPlayer.gd
+++ Player/GravityPlayer.gd
@@ -1,8 +1,12 @@
+# CREATE A MAIN PLAYER SCRIPT
+# THEN GRAVITYPLAYER AND CAVEPLAYER
+
 extends KinematicBody2D
 
+export var gravity = 1000
 onready var anim = $AnimatedSprite
 onready var bullet_scene = preload('res://Bullet/Bullet.tscn')
-export var speed = 128
+export var speed = Vector2(300, 450)
 var velocity = Vector2.ZERO
 export var dir = 'right'
 var v = {
@@ -23,32 +27,25 @@ var filling: bool = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed('ui_right'):
-		velocity.x = speed
-		velocity.y = 0
+		velocity.x = speed.x
 		anim.play('right')
 		dir = 'right'
 	elif Input.is_action_pressed('ui_left'):
-		velocity.x = -speed
-		velocity.y = 0
+		velocity.x = -speed.x
 		anim.play('left')
 		dir = 'left'
-	elif Input.is_action_pressed('ui_up'):
-		velocity.x = 0
-		velocity.y = -speed
-		anim.play('up')
-		dir = 'up'
-	elif Input.is_action_pressed('ui_down'):
-		velocity.x = 0
-		velocity = speed
-		anim.play('down')
-		dir = 'down'
 	else:
-		velocity = Vector2.ZERO
+		velocity.x = 0
 		anim.play('idle-' + dir)
+	
+	velocity.y += gravity * delta
+	
+	if is_on_floor() and Input.is_action_just_pressed('ui_up'):
+		velocity.y = -speed.y
 		
 	if Input.is_action_just_pressed('ui_accept'):
 		shoot()
-		
+
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	position = position.round()
 
@@ -116,3 +113,4 @@ func _on_PieceDetector_body_entered(body: Node) -> void:
 		$SFX/pulse.stop()
 		emit_signal('max_health', 100)
 		emit_signal('max_mp', 100)
+
